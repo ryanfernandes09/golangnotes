586110ba41f8634845d7f1a0b5194d803acbc69b
wrektangle <ryanfernandes09@gmail.com>
2020-05-18 22:36:21 -0400

    callback completed

--- src/callback.go
+++ src/callback.go
@@ -7,9 +7,9 @@ import (
 func callback() {
 
 	randomNumbers := []int{43, 25, 653, 12, 9, 0}
-	s1 := sum(randomNumbers...)
+	fmt.Println("Sum: ", sum(randomNumbers...))
 
-	fmt.Println("Sum: ", s1)
+	fmt.Println("Sum of even numbers: ", even(sum,randomNumbers...))
 
 }
 
@@ -19,4 +19,14 @@ func sum(xi ...int) int {
 		total += v
 	}
 	return total
+}
+
+func even(f func(xi ...int) int, vi ...int) int{
+	var evenNumbers []int
+	for _, v := range vi {
+		if v % 2 == 0 {
+			evenNumbers = append(evenNumbers, v)
+		}
+	}
+	return f(evenNumbers...)
 }
